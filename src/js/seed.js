3e5c9dced09876b62ecee0176081325fdf47cc7b
Eric-Pedro <ericped7@gmail.com>
2023-12-11 18:47:41 -0300

    alterações produtos.html e seed.js

--- src/js/seed.js
+++ src/js/seed.js
@@ -5,12 +5,62 @@ const seed = {
       {
         "id": "da39a3ee5e6b4b0d3255bfef95601890afd80709",
         "name": "Produto teste",
-        "category" : "capas",
+        "category" : "Capas",
         "quantity": 50,
         "brand": "Samsung",
         "description": "",
-        "price": 120,
-        "img": ""
+        "price": "120,00",
+        "img": "https://placehold.co/400"
+      },
+      {
+        "id": "da39a3ee5e6b4b0d3255bfef95601890afd8070",
+        "name": "Iphone 6s",
+        "category" : "Celular",
+        "quantity": 50,
+        "brand": "Apple",
+        "description": "",
+        "price": "1800,00",
+        "img": "https://tendinfo.com.br/image/cache/catalog/Produtos%202019/00%201052-400x400.jpg"
+      },
+      {
+        "id": "da39a3ee5e6b4b0d3255bfef95601890afd8070",
+        "name": "Caixa de Som Megaboom 3",
+        "category" : "Caixa de som",
+        "quantity": 50,
+        "brand": "Ultimate Ears",
+        "description": "",
+        "price": "465,20",
+        "img": "https://tendinfo.com.br/image/cache/catalog/0000001%20abril%202022/08.04%201-400x400.jpg"
+      },
+      {
+        "id": "da39a3ee5e6b4b0d3255bfef95601890afd8070",
+        "name": "CARTÃO DE MEMÓRIA 16GB",
+        "category" : "Cartão de memória",
+        "quantity": 50,
+        "brand": "SanDisk",
+        "description": "",
+        "price": "34,00",
+        "img": "https://tendinfo.com.br/image/cache/catalog/produtos/SDC4%20-%2016GB-400x400.jpg"
+      },
+      {
+        "id": "da39a3ee5e6b4b0d3255bfef95601890afd8070",
+        "name": "FONE DE OUVIDO MOD.SV5333",
+        "category" : "Fone de ouvido",
+        "quantity": 50,
+        "brand": "One for All",
+        "description": "",
+        "price": "48,53",
+        "img": "https://tendinfo.com.br/image/cache/catalog/produtos/sv5333-thumb-400x400.png"
+      },
+      {
+        "id": "da39a3ee5e6b4b0d3255bfef95601890afd8070",
+        "name": "URBEATS3 DEFIANT",
+        "category" : "Fone de ouvido",
+        "quantity": 50,
+        "brand": "Beats by Dr. Dre",
+        "description": "",
+        "price": "741,00",
+        "img": "https://tendinfo.com.br/image/cache/catalog/02823/0.100%20326-400x400.jpg"
       }
     ],
   },
