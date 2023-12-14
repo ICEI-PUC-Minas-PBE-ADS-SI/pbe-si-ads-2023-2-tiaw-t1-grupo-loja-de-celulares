f074b3cf8ca0a1ccf0ae52d4e9b8bdd3057362b3
leolopes2001 <leomartins2001@gmail.com>
2023-12-14 18:46:03 -0300

    Finalizando tela de login/registro/home/cart

---
+++ index.js
@@ -0,0 +1,137 @@
+const databaseName = "loja-de-celulares-database";
+let db;
+
+const productsList = document.querySelector("#productsLitst");
+
+const intProject = () => {
+  let foundDb = localStorage.getItem(databaseName);
+
+  if (!foundDb) {
+    localStorage.setItem(databaseName, JSON.stringify(seed));
+
+    foundDb = localStorage.getItem(databaseName);
+  }
+
+  db = JSON.parse(foundDb);
+};
+
+const addItemCart = (id) => {
+  if (localStorage.getItem("userLoginId")) {
+    let foundDb = localStorage.getItem(databaseName);
+
+    if (!foundDb) {
+      localStorage.setItem(databaseName, JSON.stringify(seed));
+
+      foundDb = localStorage.getItem(databaseName);
+    }
+    db = JSON.parse(foundDb);
+
+    const userFoundIdx = db.users.findIndex(
+      (user) => user.id == localStorage.getItem("userLoginId")
+    );
+
+    if (userFoundIdx) {
+      if (db.users[userFoundIdx].cart.find((item) => item.id == id)) {
+        Toast.fire({
+          icon: "warning",
+          title: "Esse produto já existe no carrinho.",
+        });
+        return;
+      }
+
+      db.users[userFoundIdx].cart.push({
+        id: id,
+        quantity: 1,
+      });
+
+      localStorage.setItem(databaseName, JSON.stringify(db));
+      
+      renderUserInfo()
+
+      Toast.fire({
+        icon: "success",
+        title:
+          "Produto adicionado no carrinho com sucesso!",
+      });
+
+    }
+  } else {
+    Toast.fire({
+      icon: "warning",
+      title:
+        "Você precisar estar autenticado para adicionar um produto ao carrinho.",
+    });
+  }
+};
+
+const renderProducts = () => {
+  const products = db.stock.products;
+  productsList.innerHTML = "";
+  products.forEach((product) => {
+    productsList.insertAdjacentHTML(
+      "beforeend",
+      `
+                <li
+                class="product bg-white col-12 col-md-6 col-xl-4"
+                data-aos="fade-up">
+                    <div class="product-img-container overflow-hidden p-3">
+                        <a href="/src/pages/Client/product.html">
+                            <img src="${product.img}" alt="Product image" class="img-fluid" />
+                        </a>
+                    </div>
+                    <div class="d-flex flex-column justify-content-between product-content-container gap-3"
+                    >
+                        <span class="product-brand text-white"> ${product.category} </span>
+                        <h6 class="product-title m-0">${product.name}</h6>
+                        <p class="product-description m-0">
+                            ${product.description}
+                        </p>
+                        <p class="product-price m-0">R$ ${product.price}</p>
+                        <button class="btn m-0 p-0 border-0" onclick="addItemCart('${product.id}')">
+                            Adicionar ao carrinho
+                        </button>
+                    </div>
+                </li>
+        `
+    );
+  });
+};
+
+const renderUserInfo = () => {
+  const userLoginId = localStorage.getItem("userLoginId");
+  if (!userLoginId) {
+    document.querySelector("#user-info-container").innerHTML = `
+        <a href="/src/pages/Auth/login.html" class="fw-bold text-white">Login</a>
+                /
+        <a href="/src/pages/Auth/register.html" class="fw-bold text-white">Cadastro</a>
+    `;
+    return;
+  }
+
+  let foundDb = localStorage.getItem(databaseName);
+
+  if (!foundDb) {
+    localStorage.setItem(databaseName, JSON.stringify(seed));
+
+    foundDb = localStorage.getItem(databaseName);
+  }
+
+  db = JSON.parse(foundDb);
+
+  const userFound = db.users.find((user) => user.id === userLoginId);
+
+  if (userFound) {
+    document.querySelector("#user-info-container").innerHTML = userFound.name;
+    document.querySelector(" #count-cart").innerHTML = userFound.cart.length;
+  }
+};
+
+intProject();
+renderProducts();
+renderUserInfo();
+
+document.querySelector("#logout").addEventListener("click", function () {
+  localStorage.setItem("userLoginId", null);
+
+  window.location = "/src/pages/Auth/login.html";
+});
--- src/js/seed.js
+++ src/js/seed.js
@@ -1,36 +1,58 @@
 const seed = {
   stock: {
-
     products: [
       {
-        "id": "da39a3ee5e6b4b0d3255bfef95601890afd80709",
-        "name": "Produto teste",
-        "category" : "capas",
-        "brand": "Samsung",
-        "description": "",
-        "price": 120,
-        "img": ""
-      }
+        id: "da39a3ee5",
+        name: "Iphone 6s",
+        category: "Celular",
+        brand: "Apple",
+        description:
+          "O iPhone 6s é um smartphone desenvolvido pela Apple e lançado em setembro de 2015.",
+        price: 1800,
+        img: "https://tendinfo.com.br/image/cache/catalog/Produtos%202019/00%201052-400x400.jpg",
+      },
+      {
+        id: "akfjienqd",
+        name: "Iphone 11",
+        category: "Celular",
+        brand: "Apple",
+        description:
+          "O iPhone 11 é um smartphone desenvolvido pela Apple e lançado em setembro de 2019.",
+        price: 3400,
+        img: "https://i.ebayimg.com/images/g/xEwAAOSwv81kNc7l/s-l1600.jpg",
+      },
+      {
+        id: "12fffeeediuwjd",
+        name: "Iphone 14",
+        category: "Celular",
+        brand: "Apple",
+        description:
+          "O iPhone 14 é um smartphone desenvolvido pela Apple e lançado em setembro de 2019.",
+        price: 4400,
+        img: "https://i.ebayimg.com/images/g/ExQAAOSwOWJjKX34/s-l1600.jpg",
+      },
     ],
   },
   users: [
     {
       id: "40bd001563085fc35165329ea1ff5c5ecbdbbeef",
-      name: "NOME DO TIO DO CLEBER",
+      email: "admin@gmail.com",
+      password: "1234",
+      name: "TIO DO CLEBER",
       isAdm: true,
     },
     {
       id: "7c4a8d09ca3762af61e59520943dc26494f8941b",
       name: "Leonardo",
+      email: "leo@gmail.com",
+      password: "1234",
       isAdm: false,
       cart: [
         {
-          "productId": "da39a3ee5e6b4b0d3255bfef95601890afd80709",
-          "quantity": 2
-        }
+          id: "da39a3ee5",
+          quantity: 2,
+        },
       ],
     },
   ],
-
-  
 };
